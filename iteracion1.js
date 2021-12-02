27e3f72083b7008c07661577be7a1a9fdd6bc7d9
Hugo Quintero <hugoquinterogarcia@gmail.com>
2021-12-02 11:27:39 +0100

    iteracion 1

---
+++ iteracion1.js
@@ -0,0 +1,15 @@
+//opción 1
+function sum (numberOne, numberTwo) {
+    return Math.max(numberOne, numberTwo)
+
+}
+let solution = (9, 32)
+console.log(solution)
+
+//opción 2
+function sum1 (numberOne, numberTwo) {
+    if (numberOne > numberTwo) {
+        console.log(numberOne)
+    } else console.log(numberTwo)
+}
+sum1(5, 9)
